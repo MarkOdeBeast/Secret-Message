19e32ded0d2958fb39c6807e61d644256616b534
Mark O'deBeast <80601021+Skatan666@users.noreply.github.com>
2022-07-16 22:18:16 +0200

    Add files via upload

---
+++ Secret Message/Components/app.js
@@ -0,0 +1,18 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop()
+console.log(secretMessage.length)
+secretMessage.push('to', 'Program')
+secretMessage['easily'] = 'right';
+secretMessage.shift()
+secretMessage.unshift('Programming')
+secretMessage.splice('get', 'right', 'the', 'first', 'time', 'know')
+console.log(secretMessage.join(' '))
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop()
+console.log(secretMessage.length)
+secretMessage.push('to', 'Program')
+secretMessage['easily'] = 'right';
+secretMessage.shift()
+secretMessage.unshift('Programming')
+secretMessage.splice('get', 'right', 'the', 'first', 'time', 'know')
+console.log(secretMessage.join(' '))
